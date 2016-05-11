e7e318230a4ed2e6e85cddee5441b0921e8c86f8
Flying-Henanese <ZhouShujian@vlan-2651-10-16-210-62.student.wireless.sydney.edu.au>
2016-05-11 16:22:30 +1000

    branch
    a branch for simulation

--- a.py
+++
@@ -1,2 +0,0 @@
-horizontal = [500+i*15 for i in range(0,21)]
-print horizontal
--- fog.py
+++
@@ -1,9 +0,0 @@
-import simpy
-import random
-
-def fog(env, pipe):
-	while True:
-		task = pipe.get()
-		computationCost = task['ComputationCost']
-		yield env.timeout(computationCost)
-		lifeTime = env.now-task['TimeStamp']
